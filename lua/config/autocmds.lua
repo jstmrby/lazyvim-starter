70c4c778db6aab368ac5b8c6215ab15ba947b520
jstmrby <48553432+jstmrby@users.noreply.github.com>
2024-03-11 23:09:06 +0100

    add custom rules

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,6 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_command("autocmd BufRead,BufNewFile *.gotmpl setfiletype gotmpl")
+vim.api.nvim_command("autocmd BufRead,BufNewFile *.gohtml setfiletype gotmpl")
