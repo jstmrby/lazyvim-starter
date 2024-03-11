0ae06279cea8f31539a858ff6ece158d3f22e720
jstmrby <48553432+jstmrby@users.noreply.github.com>
2024-03-12 00:43:45 +0100

    custom: add some keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,5 +2,20 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<C-U>", "<C-U>zz", { desc = "Jump half page up" })
-vim.keymap.set("n", "<C-D>", "<C-D>zz", { desc = "Jump half page down" })
+local map = vim.keymap.set
+
+-- Move highlighted lines
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Keep the cursor centered
+map("n", "<C-d>", "<C-d>zz", { desc = "Jump half page down" })
+map("n", "<C-u>", "<C-u>zz", { desc = "Jump half page up" })
+
+-- System clipboard
+map("n", "<leader>y", '"+y', { desc = "[y]ank to system clipboard" })
+map("v", "<leader>y", '"+y', { desc = "[y]ank to system clipboard" })
+map("n", "<leader>Y", '"+Y', { desc = "[Y]ank to system clipboard" })
+
+map("n", "<leader>p", '"+p', { desc = "[p]aste from system clipboard" })
+map("n", "<leader>P", '"+P', { desc = "[P]aste from system clipboard" })
