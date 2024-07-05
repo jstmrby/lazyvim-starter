c6d8b9038cd0df55240978d2aec41621dd1578c9
Oliver Tari <oliver.tari@supercharge.io>
2024-07-05 15:58:38 +0200

    custom: change vtsls import specifiers

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -2,11 +2,22 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     servers = {
-      tsserver = {
-        init_options = {
-          preferences = {
-            importModuleSpecifierPreference = "relative",
-            importModuleSpecifierEnding = "minimal",
+      -- tsserver = {
+      --   enabled = true,
+      --   init_options = {
+      --     preferences = {
+      --       importModuleSpecifierPreference = "project-relative",
+      --       importModuleSpecifierEnding = "minimal",
+      --     },
+      --   },
+      -- },
+      vtsls = {
+        settings = {
+          typescript = {
+            preferences = {
+              importModuleSpecifier = "relative",
+              importModuleSpecifierEnding = "minimal",
+            },
           },
         },
       },
